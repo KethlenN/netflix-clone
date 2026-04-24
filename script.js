9d817f842b62ed4c4112b839a86ed8106d199d24
Kethlen <nkethlen33@gmail.com>
2026-04-24 16:09:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const apiKey = "61ab23b62a2a8a0dd8d16aaef154f373";
-fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=27`, {
-  method: "GET",
-  headers: {
-    "Content-Type": "application/json"
-  }
-})
+const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=27`;
+
+fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent(url)}`)
   .then(res => res.json())
   .then(data => {
 
@@ -18,23 +15,12 @@ fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres
       }
     });
 
-    if (data.results.length > 0) {
-      const filme = data.results[0];
+    const filme = data.results[0];
 
-      document.getElementById("banner").style.backgroundImage =
-        `url(https://image.tmdb.org/t/p/original${filme.backdrop_path})`;
+    document.getElementById("banner").style.backgroundImage =
+      `url(https://image.tmdb.org/t/p/original${filme.backdrop_path})`;
 
-      document.getElementById("titulo").innerText = filme.title;
-      document.getElementById("descricao").innerText = filme.overview;
-    }
+    document.getElementById("titulo").innerText = filme.title;
+    document.getElementById("descricao").innerText = filme.overview;
 
-  })
-  .catch(err => console.log("ERRO:", err));
-
-console.log("JS CARREGOU");
-
-// TESTE SIMPLES SEM API
-document.getElementById("titulo").innerText = "FILME TESTE";
-document.getElementById("descricao").innerText = "Isso é um teste funcionando";
-document.getElementById("banner").style.backgroundImage =
-  "url(https://images.unsplash.com/photo-1519681393784-d120267933ba)";
+  });
