3f366ee153697e08e3fbb05bfc5a028332bee9c6
Kethlen <nkethlen33@gmail.com>
2026-04-24 16:54:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 const TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MWFiMjNiNjJhMmE4YTBkZDhkMTZhYWVmMTU0ZjM3MyIsIm5iZiI6MTc3NzA1MTkxMy45LCJzdWIiOiI2OWViYTkwOTljOTk4ODMwNTlkNDgzODAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.RnWia2POc1yDhJuOq2GF8MT2CUoijPjMemDW6PgR7ho";
-
 fetch('https://api.themoviedb.org/3/discover/movie?with_genres=27', {
   headers: {
     Authorization: `Bearer ${TOKEN}`,
     'Content-Type': 'application/json;charset=utf-8'
   }
 })
-.then(res => res.json())
+.then(res => {
+  if (!res.ok) {
+    throw new Error("Erro na API: " + res.status);
+  }
+  return res.json();
+})
 .then(data => {
+  console.log(data); // 👈 VEJA ISSO NO CONSOLE
+
   const moviesDiv = document.getElementById('movies');
 
   data.results.forEach(movie => {
+    if (!movie.poster_path) return; // evita erro
+
     const div = document.createElement('div');
     div.classList.add('movie');
 
@@ -20,4 +28,7 @@ fetch('https://api.themoviedb.org/3/discover/movie?with_genres=27', {
 
     moviesDiv.appendChild(div);
   });
+})
+.catch(error => {
+  console.error("DEU ERRO:", error);
 });
