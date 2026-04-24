5b7cacc111ba3e986662196c4bfbb4f1d344ea1b
Kethlen <nkethlen33@gmail.com>
2026-04-24 16:48:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-fetch('https://seu-backend.com/filmes')
-  .then(res => res.json())
-  .then(data => console.log(data));
-const API_KEY = process.env.API_KEY;
+const TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MWFiMjNiNjJhMmE4YTBkZDhkMTZhYWVmMTU0ZjM3MyIsIm5iZiI6MTc3NzA1MTkxMy45LCJzdWIiOiI2OWViYTkwOTljOTk4ODMwNTlkNDgzODAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.RnWia2POc1yDhJuOq2GF8MT2CUoijPjMemDW6PgR7ho";
+
+fetch('https://api.themoviedb.org/3/discover/movie?with_genres=27', {
+  headers: {
+    Authorization: `Bearer ${TOKEN}`,
+    'Content-Type': 'application/json;charset=utf-8'
+  }
+})
+.then(res => res.json())
+.then(data => {
+  const moviesDiv = document.getElementById('movies');
+
+  data.results.forEach(movie => {
+    const div = document.createElement('div');
+    div.classList.add('movie');
+
+    div.innerHTML = `
+      <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
+    `;
+
+    moviesDiv.appendChild(div);
+  });
+});
