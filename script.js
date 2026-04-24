1f7944bbbd3bccac219bd6e79f000606c198f0a8
Kethlen <nkethlen33@gmail.com>
2026-04-24 14:46:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-fetch('https://api.themoviedb.org/3/movie/popular?api_key=SUA_KEY')
+const apiKey = "61ab23b62a2a8a0dd8d16aaef154f373";
+
+fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=27`)
   .then(res => res.json())
   .then(data => {
-    console.log(data.results);
+    const filmesDiv = document.getElementById("filmes");
+
+    data.results.forEach(filme => {
+      const img = document.createElement("img");
+      img.src = `https://image.tmdb.org/t/p/w500${filme.poster_path}`;
+      filmesDiv.appendChild(img);
+    });
   });
