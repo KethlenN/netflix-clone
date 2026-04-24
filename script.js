0bbd9198c31a91146a923b7283784e03eb9801a2
Kethlen <nkethlen33@gmail.com>
2026-04-24 16:17:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-const apiKey = "61ab23b62a2a8a0dd8d16aaef154f373";
-
-app.get('/filmes', async (req, res) => {
-  const response = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=SUA_KEY&with_genres=27');
-  const data = await response.json();
-  res.json(data);
-});
-
-fetch('/filmes')
+fetch('https://seu-backend.com/filmes')
+  .then(res => res.json())
+  .then(data => console.log(data));
+const API_KEY = process.env.API_KEY;
